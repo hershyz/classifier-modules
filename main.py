6b6a7e9cab58cade061a1594b0a7d90f839616aa
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2022-02-20 01:51:39 -0500

    #babbed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 # read data
 f = open('water_potability.csv', 'r')
 lines = f.readlines()
@@ -12,3 +15,60 @@ for i in range(1, len(lines)):
         if cat not in cats:
             cats.append(cat)
 
+# make predictions
+def predict(point):
+
+    distances = {}
+    totals = {}
+    for cat in cats:
+        distances[cat] = 0
+        totals[cat] = 0
+
+    for i in range(1, len(lines)):
+        
+        comparison = lines[i].replace('\n', '').split(',')
+        comparison_cat = comparison[len(comparison) - 1]
+        
+        dist = 0
+        for j in range(0, len(point) - 1):    
+            try:
+                num = float(point[j])
+                comparison_num = float(comparison[j])
+                dist += (comparison_num - num) ** 2
+            except:
+                continue
+        distances[comparison_cat] = distances[comparison_cat] + math.sqrt(dist)
+        totals[comparison_cat] = totals[comparison_cat] + 1
+        
+    min = distances[cats[0]] / totals[cats[0]]
+    min_cat = 0
+    for cat in cats:
+        curr = distances[cat] / totals[cat]
+        if curr < min:
+            min = curr
+            min_cat = cat
+    
+    return min_cat
+
+
+
+
+
+# -----------------------
+points = []
+for i in range(0, 100):
+    points.append(random.randint(1, len(lines) - 1))
+
+total = 0
+correct = 0
+
+for i in range(0, len(points)):
+    point = lines[points[i]].replace('\n', '').split(',')
+    real = str(point[len(point) - 1])
+    prediction = str(predict(point))
+    if real == prediction:
+        correct += 1
+    total += 1
+
+acc = (correct / total) * 100
+print('accuracy: ' + str(acc))
